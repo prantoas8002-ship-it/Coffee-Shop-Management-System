9d328e45c1e32a8b22d83dcdc0db6c3eda753694
Pranto Ghosh <prantoas8002@gmail.com>
2026-08-21 12:31:26 +0600

    productPage render

--- src/controller/dashBoardController.java
+++ src/controller/dashBoardController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+
 public class dashBoardController {
+
+    @FXML
+    private void openProducts(ActionEvent event) {
+        try {
+            FXMLLoader loader =
+                    new FXMLLoader(getClass().getResource("/view/Product.fxml"));
+
+            Parent root = loader.load();
+
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
